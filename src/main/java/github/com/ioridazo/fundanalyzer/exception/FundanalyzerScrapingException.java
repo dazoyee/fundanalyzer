48e6bcc3bc7be11775ba366f0d9e9a004c232b36
iori-oiso <iori.dazo@gmail.com>
2021-02-27 14:41:50 +0900

    株価取得処理のロジックを更生した

---
+++ src/main/java/github/com/ioridazo/fundanalyzer/exception/FundanalyzerScrapingException.java
@@ -0,0 +1,8 @@
+package github.com.ioridazo.fundanalyzer.exception;
+
+public class FundanalyzerScrapingException extends RuntimeException {
+
+    public FundanalyzerScrapingException(final Throwable cause) {
+        super(cause);
+    }
+}
