ce9b94efa41bb928ceb05eb46d8006210b2b982e
iori-oiso <iori.dazo@gmail.com>
2021-06-06 13:05:56 +0900

    corporate画面の提出日を降順にした

--- src/main/java/github/com/ioridazo/fundanalyzer/web/view/model/corporate/detail/FinancialStatementViewModel.java
+++ src/main/java/github/com/ioridazo/fundanalyzer/web/view/model/corporate/detail/FinancialStatementViewModel.java
@@ -2,11 +2,13 @@ package github.com.ioridazo.fundanalyzer.web.view.model.corporate.detail;
 
 import lombok.Value;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SuppressWarnings("RedundantModifiersValueLombok")
 @Value(staticConstructor = "of")
 public class FinancialStatementViewModel {
+    private final LocalDate submitDate;
     private final FinancialStatementKeyViewModel key;
     private final List<FinancialStatementValueViewModel> bs;
     private final List<FinancialStatementValueViewModel> pl;
