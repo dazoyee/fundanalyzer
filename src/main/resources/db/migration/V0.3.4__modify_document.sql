19d1e9fd58d048a4c961899ac552a02afe0e76dd
iori-oiso <iori.dazo@gmail.com>
2023-03-20 23:49:53 +0900

    spring boot 2.7

--- src/main/resources/db/migration/V0.3.4__modify_document.sql
+++ src/main/resources/db/migration/V0.3.4__modify_document.sql
@@ -1,2 +1,2 @@
-ALTER TABLE IF EXISTS `document` MODIFY COLUMN `downloaded` CHAR (1) NOT NULL DEFAULT '0' COMMENT 'ダウンロードステータス' CHECK (`downloaded` IN ('0', '1', '5', '9'));
-ALTER TABLE IF EXISTS `document` MODIFY COLUMN `decoded` CHAR (1) NOT NULL DEFAULT '0' COMMENT 'ファイル解凍ステータス' CHECK (`decoded` IN ('0', '1', '5', '9'));
+ALTER TABLE IF EXISTS `document` ADD CONSTRAINT `downloaded_check` CHECK (`downloaded` IN ('0', '1', '5', '9'));
+ALTER TABLE IF EXISTS `document` ADD CONSTRAINT `decoded_check` CHECK (`decoded` IN ('0', '1', '5', '9'));
